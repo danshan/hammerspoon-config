a4a90018ca81b654e2559193c913b2c08e37a2c4
shanhonghao <i@shanhh.com>
2020-02-05 14:53:21 +0800

    feature check nil before move mouse position

--- modules/apps.lua
+++ modules/apps.lua
@@ -1,7 +1,12 @@
 function launchApp(basicKey, object)
     hs.hotkey.bind(basicKey, object.key, function() 
         hs.application.launchOrFocus(object.app)
-        local window = hs.application.get(object.app):focusedWindow()
-        moveToCenterOfWindow(window)
+        local application = hs.application.get(object.app)
+        if application ~= nil then
+            local window = application:focusedWindow()
+            if window ~= nil then
+                moveToCenterOfWindow(window)
+            end
+        end
     end)
 end
